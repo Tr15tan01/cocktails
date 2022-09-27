ece65c8d37d610a07e44c7bd862bddbe9fd94994
tr15tan01 <t.varamashvili@gmail.com>
2022-09-27 21:53:44 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import pages
+// import Home from './pages/Home';
+
+// import About from './pages/About';
+import Error from './pages/Error';
+import Loading from './components/Loading';
+import Navbar from './components/Navbar';
+import MyerrorBoundary from './components/MyerrorBoundary';
+
+const Home = React.lazy(() => import('./pages/Home'))
+const SingleCocktail = React.lazy(() => import('./pages/SingleCocktail'))
+const About = React.lazy(() => import('./pages/About'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Suspense fallback={<Loading />}><Home /></Suspense>} />
+
+        <Route path='about' element={<MyerrorBoundary><Suspense fallback={<Loading />}><About /></Suspense></MyerrorBoundary>} />
+
+        <Route path='cocktail/:id' element={<Suspense fallback={<Loading />}><SingleCocktail /></Suspense>} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router >
+
   );
 }
 
